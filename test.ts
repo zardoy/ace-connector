c0afa3e21b474df98849a1a195c80fc243c055bd
Vitaly Turovsky <vital2580@icloud.com>
2021-02-16 01:57:54 +0300

    done with got testing

--- test.ts
+++ test.ts
@@ -1,26 +1,3 @@
-import axios from "axios";
-import promiseRetry from "promise-retry";
-import axiosRetry from "axios-retry";
-import cheerio from "cheerio";
-
-const main = async () => {
-    axiosRetry(axios, { retries: 3 });
-    // https://stackoverflow.com/questions/14728038/disabling-the-large-file-notification-from-google-drive
-    // todo use drive-api? https://developers.google.com/drive/api/v3/manage-downloads#node.js
-    const { data, headers } = await axios.get(
-        "https://drive.google.com/uc?export=download&id=1PprErmACZnYmTPGoQST9Yy5ECYsOoLQq",
-        {
-            responseType: "stream"
-        }
-    );
-    const contentType: string = headers["content-type"];
-    if (contentType.includes("text/html")) {
-        
-    }
-}
-
-main().catch(err => {throw err})
-
 // SI PROCESS BUG
 
 // import si from "systeminformation";
