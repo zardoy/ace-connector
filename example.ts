14ab09547c0decf93e4bf3ad45090744534e212e
Vitaly Turovsky <vital2580@icloud.com>
2021-02-16 19:04:10 +0300

    add working example

--- example.ts
+++ example.ts
@@ -1,44 +1,14 @@
-import { AceConnector } from "ace-connector";
+process.env["DEBUG"] = "ace-connector";
+
+import { AceConnector } from "./src";
+
 export const aceConnector = new AceConnector();
-aceConnector.updateStatus
-const installAceStream = async () => {
-    await AceConnector.downloadInstaller();
-    // open the installer
-};
-aceEngine.on("starting", () => {
-    // engine is starting
-    // update UI if needed
-});
-aceEngine.on("connect", () => {
-    // ace engine connected successfully
-    aceEngine.version; // connected engine version
-});
-aceEngine.on("disconnect", () => {
-    // emits only if ace engine was connected
 
-    // update UI status to disconnected
-});
-aceEngine.on("confirmRequired", async ({ type, message }): Promise<boolean> => {//must be defined
-    // ace old version, ace auto updates enabled, ace patch available
-    const userDecisionResult = await userConfirmDialog(message);
-    return userDecisionResult;
-});
-const updateConnection = async () => {
-    try {
-        await aceEngine.connect();
-    } catch (err) {
-        if (!err.connectionError) throw err;
-        if (err.NeedsInstall) {
-            // suggest Ace Stream installation to user
-            // if user agree to install
-            installAceStream();
-        } else {
-            // show error to user:
-            // err.techReason - detailed description of the error
-            // err.message - error in general
-            // err.suggestion
-        }
-    }
-};
+(async () => {
+    await aceConnector.connect();
+})()
+    .catch(err => { throw err; });
 
-updateConnection().catch(err => console.error(err));
+
+// currently using for testing library
+// todo add real example
